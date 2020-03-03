46628770d84c560874b5db8b33946675e0819f32
Yaquelin-martinez <61429463+Yaquelin-martinez@users.noreply.github.com>
2020-03-02 21:49:05 -0600

    modificando variables apartado reservaciones

--- Huellita_Digital/src/main/java/com/huellitas/entities/Reservaciones.java
+++ Huellita_Digital/src/main/java/com/huellitas/entities/Reservaciones.java
@@ -13,24 +13,24 @@ public class Reservaciones {
 	private int id;
 	
 	@Column(name="reservacion_nombre_resp", length=50, unique = false)
-	private String reservacion_nombre_resp;
+	private String reservacionNombreResp;
 	
 	@Column(name="reservacion_fecha", length=10, unique = false)
-	private String reservacion_fecha;
+	private String reservacionFecha;
 	
 	@Column(name="reservacion_hora", length=8, unique = false)
-	private String reservacion_hora;
+	private String reservacionHora;
 	
 	@Column(name="reservacion_telefono_resp", length=8, unique = false)
-	private int reservacion_telefono_resp;
+	private int reservacionTelefonoResp;
 	
 	@Column(name="reservacion_cant_personas", length=11, unique = false)
-	private int reservacion_cant_personas;
+	private int reservacionCantPersonas;
 	
 	@Column(name="reservacion_estado", length=1, unique = false)
-	private char reservacion_estado;
+	private char reservacionEstado;
 	
 	@Column(name="reservacion_id_cliente", length=11, unique = false)
-	private int reservacion_id_cliente;
+	private int reservacionIdCliente;
 
 }
