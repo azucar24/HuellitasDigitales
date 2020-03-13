135b73f0208c041573efe27bef63d1d9dd9ef513
Yonathan Ortiz <ede96yona@gmail.com>
2020-03-12 20:47:49 -0600

    Avanzando controladores

--- Huellita_Digital/src/main/java/com/huellitas/controller/ControllerClientes.java
+++ Huellita_Digital/src/main/java/com/huellitas/controller/ControllerClientes.java
@@ -1,5 +1,47 @@
 package com.huellitas.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huellitas.entities.Clientes;
+
+@RestController
+@RequestMapping("huellitas/clientes")
 public class ControllerClientes {
+	@Autowired
+	private com.huellitas.models.ServiceClientes ServiceClientes;
+	
+	@RequestMapping(value="/clientes", method = RequestMethod.GET)
+		public List<Clientes> obtenerListas(){
+		List<Clientes> nombreObjeto = ServiceClientes.findAll();
+		return nombreObjeto;
+	}
+	
+	/*================================== BUSCAR POR ID ============================================*/
+	@RequestMapping(value="/clientes/{id}", method = RequestMethod.GET)
+	public Clientes obtenerPorId(@PathVariable("id") Long id){
+		java.util.Optional<Clientes> nombreObjeto = ServiceClientes.findById(id);
+		return nombreObjeto.get();
+	}
+	
+	/*=================================== AGREGAR ===============================================*/
+	@RequestMapping(value="/clientes", method = RequestMethod.POST)
+		public Clientes guardar(@RequestBody Clientes creditLine){
+		Clientes objeto = ServiceClientes.save(creditLine);
+		return objeto;
+	}
+	
+	/*================================== ELIMINAR ===============================================*/
+	@RequestMapping(value="/clientes/{id}", method = RequestMethod.GET)
+		public void delete(@PathVariable("id") Long id){
+		ServiceClientes.delete(id);
+	}
 
 }
