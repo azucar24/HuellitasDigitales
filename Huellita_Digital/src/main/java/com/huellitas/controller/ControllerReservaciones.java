5499710ac5ae982ec4366bab26c46fd1db9b26e7
Yonathan Ortiz <ede96yona@gmail.com>
2020-03-12 20:17:23 -0600

    Cambiando nombres de paquetes

--- Huellita_Digital/src/main/java/com/huellitas/controller/ControllerReservaciones.java
+++ Huellita_Digital/src/main/java/com/huellitas/controller/ControllerReservaciones.java
@@ -1,4 +1,4 @@
-package controller;
+package com.huellitas.controller;
 
 public class ControllerReservaciones {
 
--- Huellita_Digital/src/main/java/com/huellitas/models/repositoryUsuarios.java
+++ Huellita_Digital/src/main/java/com/huellitas/models/repositoryUsuarios.java
@@ -1,4 +1,4 @@
-package models;
+package com.huellitas.models;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
