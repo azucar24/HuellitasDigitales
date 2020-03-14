eedd63c85b8b76b9a66b453bdbb08239ad03bbfb
Yaquelin-martinez <61429463+Yaquelin-martinez@users.noreply.github.com>
2020-03-13 23:31:17 -0600

    Agregando controlador para las reservaciones

--- Huellita_Digital/src/main/java/com/huellitas/controller/ControllerReservaciones.java
+++ Huellita_Digital/src/main/java/com/huellitas/controller/ControllerReservaciones.java
@@ -1,5 +1,44 @@
 package com.huellitas.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huellitas.entities.Reservaciones;
+@RestController
+@RequestMapping("huellitas/reservaciones")
 
 public class ControllerReservaciones {
+	@Autowired
+	private com.huellitas.models.ServiceReservaciones ServiceReservaciones;
+	
+	@RequestMapping(value="/reservaciones", method = RequestMethod.GET)
+		public List<Reservaciones> obtenerListas(){
+		List<Reservaciones> ReservacionesID = ServiceReservaciones.findAll();
+		return ReservacionesID;
+	}
+	
+	/*================================== BUSCAR POR ID ============================================*/
+	@RequestMapping(value="/reservaciones/{id}", method = RequestMethod.GET)
+	public Reservaciones obtenerPorId(@PathVariable("id") Long id){
+		java.util.Optional<Reservaciones> ReservacionesID = ServiceReservaciones.findById(id);
+		return ReservacionesID.get();
+	}
+	
+	/*=================================== AGREGAR ===============================================*/
+	@RequestMapping(value="/reservaciones", method = RequestMethod.POST)
+	public Reservaciones guardar(@RequestBody Reservaciones creditLine){
+	Reservaciones NewReservacion = ServiceReservaciones.save(creditLine);
+	return NewReservacion;
+	}
+	
+	/*================================== ELIMINAR ===============================================*/
+	@RequestMapping(value="/reservaciones/{id}", method = RequestMethod.GET)
+		public void delete(@PathVariable("id") Long id){
+		ServiceReservaciones.delete(id);
+	}
 
 }
