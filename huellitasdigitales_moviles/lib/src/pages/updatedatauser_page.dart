9d9d98fdf8347a0e618531bcf7fc2c39b9c4f1f8
YonathanOrtiz <ede96yona@gmail.com>
2020-04-20 10:50:00 -0600

    Creando pantalla 7, app movil

--- huellitasdigitales_moviles/lib/src/pages/updatedatauser_page.dart
+++ huellitasdigitales_moviles/lib/src/pages/updatedatauser_page.dart
@@ -1,11 +1,7 @@
-
 import 'package:flutter/material.dart';
-import 'package:huellitasdigitales_moviles/src/pages/principal_page.dart';
 
-String _user = "yonathan";
-String _password = "tu90mama";
 
-class HomePage extends StatelessWidget{
+class UpdateDataUser extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +15,10 @@ class HomePage extends StatelessWidget{
             Center(
               child: Column(
                 children: <Widget>[
-                  Image(image:AssetImage("assets/images/pp.jpg"), width:300,height:300),
-                  Text("Ingresa tus datos:", style: TextStyle(fontSize: 30),),
+                  Image(image:AssetImage("assets/images/pp.jpg"), width:150,height:150),
+                  Text("Actualiza tus datos:", style: TextStyle(fontSize: 30),),
                   Divider(),
-                  cajitas(),
-                  botones(context),
+                  cajas(),
                   SizedBox(height: 5),
                   Text("San Miguel, El Salvador, Todos los derechos reservados.", style: TextStyle(fontSize: 13),),
                   Text("© Huellitas Digitales, 2020", style: TextStyle(fontSize: 13),),
@@ -35,94 +30,7 @@ class HomePage extends StatelessWidget{
         )
     );
   }
-}
-
-Widget cajitas(){
-  return Column(
-    children: <Widget>[
-      TextFormField(
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 20),
-        keyboardType: TextInputType.multiline,
-        maxLines: 1,
-        maxLength: 15,
-        decoration: InputDecoration(
-          icon: Icon(Icons.person),
-          labelText: 'Ingresa tu usuario',
-        ),
-      ),
-      SizedBox(width: 50),
-      TextFormField(
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 20),
-        keyboardType: TextInputType.multiline,
-        maxLines: 1,
-        maxLength: 15,
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(Icons.vpn_key),
-          labelText: 'Ingresa tu contraseña',
-        ),
-      )
-    ],
-  );
-}
-
-Widget botones(BuildContext context) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.end,
-    children: <Widget>[
-      RaisedButton(
-        child: Text('Entrar', style: TextStyle(fontSize: 20),),
-        color: Colors.cyan,
-        onPressed: () {
-          // Navega a la segunda ruta cuando se pulsa.
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => PrincipalPage()),
-          );
-        },
-      ),
-      SizedBox(width: 50),
-      RaisedButton(
-        child: Text('Registrarse', style: TextStyle(fontSize: 20),),
-        onPressed: () {
-          // Navega a la segunda ruta cuando se pulsa.
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => UserAdd()),
-          );
-        },
-      ),
-    ]
-  );
-}
-
-class UserAdd extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Huellita Digital, El Salvador"),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: 
-        ListView(
-          children: <Widget>[
-            Center(
-            child: Column(
-              children: <Widget>[
-                Image(image:AssetImage("assets/images/pp.jpg"), width:150,height:150),
-                Text("Registra tus datos", style: TextStyle(fontSize: 35),),
-                cajas()
-              ],
-            ) 
-          )
-          ],
-        )
-    );
-  }
-
+  
   Widget cajas() {
      return Column(
       mainAxisAlignment: MainAxisAlignment.end,
@@ -192,10 +100,6 @@ class UserAdd extends StatelessWidget{
             
           },
         ),
-        SizedBox(height: 5),
-        Text("San Miguel, El Salvador, Todos los derechos reservados.", style: TextStyle(fontSize: 13),),
-        Text("© Huellitas Digitales, 2020", style: TextStyle(fontSize: 13),),
-        SizedBox(height: 2),
       ]
     );
   }
