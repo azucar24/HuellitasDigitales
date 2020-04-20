2f37cb547125f4bc4e021d870b370e83b6a1e2bc
Yaquelin-martinez <61429463+Yaquelin-martinez@users.noreply.github.com>
2020-04-20 12:45:51 -0500

    Creando pantalla My Acount

--- huellitasdigitales_moviles/lib/src/pages/myacount_page.dart
+++ huellitasdigitales_moviles/lib/src/pages/myacount_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:huellitasdigitales_moviles/src/pages/myacount_page.dart';
 
-class PrincipalPage extends StatelessWidget {
+class MyAcount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -50,10 +49,7 @@ class PrincipalPage extends StatelessWidget {
              new ListTile(
               title: new Text('My Acount.'),
               onTap: (){
-                 Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MyAcount()),
-          );
+                Navigator.of(context).pop();
               },
             ),
              new Divider(
@@ -69,23 +65,24 @@ class PrincipalPage extends StatelessWidget {
           ]
         )
       ),
-
-
-
-
-
       body: 
         ListView(
           children : <Widget>[
             Center(
               child: Column(
                 children: <Widget>[
-                  Image(image:AssetImage("assets/images/pp.jpg"), width:300,height:300),
-                  Text("Bienvenid@ a Huellita Digital", style: TextStyle(fontSize: 30), textAlign: TextAlign.center,),
+                  Text("My Acount", style: TextStyle(fontSize: 30), textAlign: TextAlign.center,),
+                  Image(image:AssetImage("assets/images/pp.jpg"), width:300,height:300),                  
                   Divider(),
                   RaisedButton(
-                  child: Text('Reservación', style: TextStyle(fontSize: 20),),
-                  color: Colors.lightGreen,
+                  child: Text('Modificar.', style: TextStyle(fontSize: 20),),
+                  color: Colors.cyan,
+                   onPressed: () {},
+                  ),
+                   Divider(),
+                   RaisedButton(
+                  child: Text('Eliminar.', style: TextStyle(fontSize: 20),),
+                  color: Colors.cyan,
                    onPressed: () {},
                   ),
                   SizedBox(height: 5),
